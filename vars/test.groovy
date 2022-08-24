29a55ae524e5f41a3f022b6f14fc1f22a5fca02d
darkroomdrca <101974598+darkroomdrca@users.noreply.github.com>
2022-08-24 13:47:46 +0530

    Update test.groovy

--- vars/test.groovy
+++ vars/test.groovy
@@ -1,6 +1,7 @@
 #!/usr/bin/env groovy
 // vars/YourStepName.groovy
 
-def call() {
-   echo "shared"
+def start() {
+   slackSend (teamDomain: "techverito", channel: "#gocd-build-notifications", tokenCredentialId: "32065985-0e36-4265-bdd8-8fc1e942f4f4",color: "good", 
+            message: "Finished ${env.JOB_NAME} #${env.BUILD_NUMBER} by ${currentBuild.getBuildCauses()[0].userId} (<${env.BUILD_URL}|Open>)")
 }
