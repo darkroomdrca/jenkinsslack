2ac2aad94823b28d3cd76600d6dd8a06e0b94863
darkroomdrca <101974598+darkroomdrca@users.noreply.github.com>
2022-08-24 14:23:21 +0530

    Update test.groovy

--- vars/test.groovy
+++ vars/test.groovy
@@ -3,7 +3,7 @@
 
 def start() {
    slackSend (teamDomain: "techverito", channel: "#gocd-build-notifications", tokenCredentialId: "32065985-0e36-4265-bdd8-8fc1e942f4f4",color: "good", 
-            message: "Finished ${env.JOB_NAME} #${env.BUILD_NUMBER} by ${currentBuild.getBuildCauses()[0].userId} (<${env.BUILD_URL}|Open>)")
+            message: "Started ${env.JOB_NAME} #${env.BUILD_NUMBER} by ${currentBuild.getBuildCauses()[0].userId} (<${env.BUILD_URL}|Open>)")
 }
 
 def notifyBuild(String buildStatus = 'STARTED') {
