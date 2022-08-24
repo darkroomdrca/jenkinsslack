55615252ae61374881c62a084b9b7d53ae961cd1
darkroomdrca <101974598+darkroomdrca@users.noreply.github.com>
2022-08-24 16:36:49 +0530

    Update test.groovy

--- vars/test.groovy
+++ vars/test.groovy
@@ -1,7 +1,7 @@
 #!/usr/bin/env groovy
 // vars/YourStepName.groovy
 
-def notifySlack(String buildStatus = 'buildStatus' ) {
+def notifySlack(String buildStatus = currentBuild.result ) {
     // Build status of null means success.
     buildStatus = buildStatus ?: 'SUCCESS'
 
