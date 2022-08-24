d6d470b9cf9d297b1e3cd2514b6ef642233548d7
darkroomdrca <101974598+darkroomdrca@users.noreply.github.com>
2022-08-24 20:20:47 +0530

    Update test.groovy

--- vars/test.groovy
+++ vars/test.groovy
@@ -23,7 +23,7 @@
 }
 */
 def fail() {
-   slackSend (teamDomain: "techverito", channel: "#gocd-build-notifications", tokenCredentialId: "32065985-0e36-4265-bdd8-8fc1e942f4f4",color: "good", 
+   slackSend (teamDomain: "techverito", channel: "#gocd-build-notifications", tokenCredentialId: "32065985-0e36-4265-bdd8-8fc1e942f4f4",color: "bad", 
             message: "*FAILURE* ${env.JOB_NAME} #${env.BUILD_NUMBER} by ${currentBuild.getBuildCauses()[0].userId} (<${env.BUILD_URL}|Open>)")
 }
 
